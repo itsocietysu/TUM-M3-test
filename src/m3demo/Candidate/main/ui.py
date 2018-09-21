c005dea2824660dec295df0f22195dd643ec8128
Jannyss <jannyss@mail.ru>
2018-09-21 09:12:18 -1200

    Update ui.py

--- src/m3demo/Candidate/main/ui.py
+++ src/m3demo/Candidate/main/ui.py
@@ -2,6 +2,7 @@
 from objectpack.ui import ModelEditWindow
 
 from m3demo.core.utils.ui import reorder_controls
+from m3demo.core.controllers import core_observer
 
 from ..models import Candidate
 
@@ -12,12 +13,15 @@ class EditWindow(ModelEditWindow):
 
     _field_list = (
         'name',
-        'planet',
+        'planet_id',
         'age',
         'email'
     )
     field_fabric_params = dict(
         field_list=_field_list,
+        model_register=ModelRegister({
+            'Planet': __package__ + '.actions.PlanetsPack',
+        }, core_observer),
     )
 
     def _do_layout(self):
