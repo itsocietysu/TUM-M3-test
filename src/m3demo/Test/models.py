729d4b211e1c44191a55801b0da1c50e1db961ec
Igor Khalepskiy <ikhalepsky@gmail.com>
2018-09-22 20:16:33 +0300

    Update models.py

--- src/m3demo/Test/models.py
+++ src/m3demo/Test/models.py
@@ -20,3 +20,42 @@ class Test(BaseModel):
 
     def __str__(self):
         return (u'%s' % str(self.name)).encode('utf-8')
+
+
+class Question(BaseModel):
+    text = models.TextField(max_length=1000)
+
+    class Meta:
+        verbose_name = u'Question'
+        verbose_name_plural = u'Questions'
+
+    def __unicode__(self):
+        return (u'%s' % str(self.name)).encode('utf-8')
+
+    def __str__(self):
+        return (u'%s' % str(self.name)).encode('utf-8')
+
+
+class TestResult(models.Model):
+    
+    test = models.ForeignKey(
+        Test, 
+        verbose_name=u'Test', 
+        on_delete=models.CASCADE
+    )
+    
+    question = models.ForeignKey(
+        Question, 
+        on_delete=models.CASCADE
+    )
+    answer = models.BooleanField()
+    
+    class Meta:
+        verbose_name = u'TestResult'
+        verbose_name_plural = u'TestResults'
+
+    def __unicode__(self):
+        return (u'%s' % str(self.name)).encode('utf-8')
+
+    def __str__(self):
+        return (u'%s' % str(self.name)).encode('utf-8')
